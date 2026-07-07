391728f25b9c8a0eeeb76eadd6968ea5713dcbb4
gantaro <gantaro1967@gmail.com>
2026-07-07 00:25:34 +0000

    feat: add current event room service

--- src/services/room_service.rs
+++ src/services/room_service.rs
@@ -41,6 +41,12 @@ impl From<sqlx::Error> for RoomError {
     }
 }
 
+pub async fn current_event(pool: &MySqlPool) -> Result<event_repository::Event, RoomError> {
+    event_repository::find_singleton(pool)
+        .await?
+        .ok_or(RoomError::NotFound)
+}
+
 impl std::fmt::Display for RoomError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
